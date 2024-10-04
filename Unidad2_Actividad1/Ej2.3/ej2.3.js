b1c156156920c61ebd78658f658a0e744205c59f
Marina González <marinagogu4@gmail.com>
2024-10-04 12:21:49 +0200

    Ejercicio 3

--- Unidad2_Actividad1/Ej2.3/ej2.3.js
+++ Unidad2_Actividad1/Ej2.3/ej2.3.js
@@ -1,6 +1,16 @@
 function testEj2_3(){
     let num1, num2;
+    let div;
+    num1 = prompt('Introduce el num1:');
+    num2 = prompt('Introduce el num2:');
     
-    
+    /*
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    */
 
+    div = num1/num2;
+    alert("El resultado de "+num1+ " / "+num2 +" es :"+div);
+
+    //Si intentamos dividir dos textos, dara error, ya que no se pueden dividir dos Strings.
 }
