73cd428d16b8c0b4f8a5e1b1e2bc7b5aa3c7e91e
Marina González <marinagogu4@gmail.com>
2024-10-08 10:58:18 +0200

    Ej12 principio

---
+++ Unidad2_Actividad1/Ej2.12/ej2.12.js
@@ -0,0 +1,13 @@
+/*Ejercicio 2.12
+Crear un programa que lea un número entero y a partir de él cree un cuadrado de asteriscos con ese tamaño.
+*/
+function testEj2_12(){
+let num =0; 
+num = prompt("Introduce un numero :");
+for(let i= 0; i<num; i++){
+    
+document.write(" \t * \t");
+    
+    
+}
+}
