0edf6d49781271267b1d3855f50b0c7c10c7793b
Marina González <marinagogu4@gmail.com>
2024-10-09 12:33:02 +0200

    ej24

---
+++ Unidad2_Actividad1/Ej2.14/ej2.14.js
@@ -0,0 +1,6 @@
+/*Hacer un programa que pida un número, luego pide a tu compañero que intente adivinarlo.
+ Tienes 5 oportunidades. Utiliza la sentencia break para salir del bucle si lo adivina antes.
+ */
+function testEj2_14(){
+
+}
